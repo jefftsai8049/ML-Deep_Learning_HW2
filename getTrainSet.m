3e71590d57e99fe2625259c1d60561395a4f4322
Jeff Tsai <jefftsai8049@gmail.com>
2015-04-22 10:14:21 +0800

    import feature vector ok

--- getTrainSet.m
+++ getTrainSet.m
@@ -1,13 +1,17 @@
-function [patterns labels] = getTrainSet(trainData,range,count)
-if count ==1
-    from = 1;
-    to = sum(range(1:count));
+function [patterns labels] = getTrainSet(trainData,range,startRow,endRow)
+if startRow ==1
+
+    delta = 0;
 
 else
-    from = sum(range(1:count-1))+1;
-    to = sum(range(1:count));
+    delta = sum(range(1:startRow-1));
 end
+
+from = delta+1;
+to = delta+sum(range(startRow:endRow));
+
 trainSet = trainData(from:to,:);
+
 labels = num2cell(trainSet(:,1)');
 % Cut patterns
 patterns = num2cell(trainSet(:,2:size(trainSet,2))',[1,size(trainSet,2)]);
--- main.m
+++ main.m
@@ -7,14 +7,8 @@ range = csvread('range.csv');
 % trainData = importTrain('train_sorted.csv',1,inf);
 % (if load all data )
 trainData = importTrain('train_sorted.csv',1,1000);
-% Cut a Set (a segment)
-% trainSet = getTrainSet(trainData,range,1);
-% Cut labels
-% labels = num2cell(trainSet(:,1)');
-% Cut patterns
-% patterns = num2cell(trainSet(:,2:size(trainSet,2))',[1,size(trainSet,2)]);
-
-[patterns labels] = getTrainSet(trainData,range,1);
+featureVector = importFeatureVector('feature_vector.csv',1,1);
+[patterns labels] = getTrainSet(trainData,range,1,1);
 %%
 %Initail GPU Device
 gpu = gpuDevice;
@@ -31,4 +25,4 @@ svmParameters.dimension = 69 ;
 % What ?
 svmParameters.verbose = 1 ;
 svmModel = svm_struct_learn(' -c 1.0 -o 1 -v 1 ', svmParameters) ;
-w = model.w ;
+w = svmModel.w ;
